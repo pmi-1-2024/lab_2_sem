92eee8a734b378502073d476f6119ba4184b4073
SofiiaShelep <sshelep5@gmail.com>
2025-03-07 08:58:13 +0200

    added hw01

---
+++ lab01/SofiiaShelep/ConsoleApplication4.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <fstream>
+#include "Group.h"
+
+using namespace std;
+
+Group* groups = nullptr;
+int groupCount = 0;
+
+void loadGroups() {
+    ifstream file("students.txt");
+    if (!file.is_open()) {
+        cout << "Error: File not found!" << endl;
+        return;
+    }
+
+    file >> groupCount;
+    groups = new Group[groupCount];
+
+    for (int i = 0; i < groupCount; i++) {
+        string groupName;
+        int studentCount;
+        file >> groupName >> studentCount;
+        cout << studentCount << endl;
+        groups[i] = Group(groupName, studentCount);
+
+        for (int j = 0; j < studentCount; j++) {
+            Student student;
+            file >> student;
+            groups[i].addStudent(student);
+        }
+    }
+    file.close();
+}
+
+int main() {
+    loadGroups();
+
+    if (groupCount == 0) {
+        cout << "No groups loaded." << endl;
+    }
+    else {
+        for (int i = 0; i < groupCount; i++) {
+            groups[i].showGroup();
+        }
+    }
+
+    delete[] groups;
+    return 0;
+}
---
+++ lab01/SofiiaShelep/Student.cpp
@@ -0,0 +1,40 @@
+#include "Student.h"
+#include <iostream>
+
+Student::Student() : name(""), group(""), address(), subject1(), subject2() {}
+
+Student::Student(string n, string g, Address a, Subject s1, Subject s2)
+    : name(n), group(g), address(a), subject1(s1), subject2(s2) {
+}
+
+void Student::setName(string n) { name = n; }
+void Student::setGroup(string g) { group = g; }
+void Student::setAddress(Address a) { address = a; }
+void Student::setSubjects(Subject s1, Subject s2) { subject1 = s1; subject2 = s2; }
+
+string Student::getName() const { return name; }
+string Student::getGroup() const { return group; }
+Address Student::getAddress() const { return address; }
+Subject Student::getSubject1() const { return subject1; }
+Subject Student::getSubject2() const { return subject2; }
+
+void Student::show() const
+{
+    cout << "Name: " << name << endl;
+    cout << "Group: " << group << endl;
+    cout << "Address: " << address << endl;
+    cout << "Subject1: " << subject1 << endl;
+    cout << "Subject2: " << subject2 << endl;
+}
+
+ostream& operator<<(ostream& os, const Student& student)
+{
+    os << student.name << " " << student.group << " " << student.address << " " << student.subject1 << " " << student.subject2;
+    return os;
+}
+
+istream& operator>>(istream& is, Student& student)
+{
+    is >> student.name >> student.group >> student.address >> student.subject1 >> student.subject2;
+    return is;
+}
