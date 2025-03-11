5cdbe0d9bea8db0fd188eeb4a55a9f66000faf44
Mark <bagenmar@gmail.com>
2025-03-11 21:52:42 +0200

    lab2

---
+++ lab02/Bahen/src/r_phone.cpp
@@ -0,0 +1,21 @@
+#include "r_phone.h"
+
+RadioPhone::RadioPhone() :Phone() {
+	this->radius = 100;
+	this->is_auto = false;
+}
+bool RadioPhone::has_auto(){
+	return is_auto;
+}
+string RadioPhone::get_type(){
+	return "R";
+}
+
+void RadioPhone::print(ostream& out) {
+	out << firm + " " + name + " " + to_string(radius) + "km " + "answering machine:" + to_string(is_auto) + " price:" + to_string(price) + "usd"<<endl;
+}
+
+void RadioPhone ::input(istream& in){
+	in >> firm >> name >> price >> radius >> is_auto;
+}
+
