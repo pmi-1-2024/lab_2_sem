ebf6945614c3bbfa9a8de8ea63c8a8f6a70a330b
Yakymovych.Taras <akimovictaras066@gmail.com>
2025-03-21 12:25:31 +0200

    optimization

---
+++ lab_02/Yakymovych_02/main.cpp
@@ -0,0 +1,62 @@
+#include "Phone.h"
+#include "MobilePhone.h"
+#include "RadioPhone.h"
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+
+int main() {
+    ifstream file1("phones1.txt");
+    ifstream file2("phones2.txt");
+    ofstream output("res.txt");
+
+    if (!file1 || !file2 || !output) {
+        cerr << "Error opening file!" << endl;
+        return 1;
+    }
+
+    int size = 0;
+    Phone** phones = new Phone * [100];
+    int type;
+
+    while (file1 >> type) {
+        if (type == 1)
+            phones[size] = new MobilePhone;
+        else if (type == 2)
+            phones[size] = new RadioPhone;
+        file1 >> *phones[size];
+        size++;
+    }
+    while (file2 >> type) {
+        if (type == 1)
+            phones[size] = new MobilePhone;
+        else if (type == 2)
+            phones[size] = new RadioPhone;
+        file2 >> *phones[size];
+        size++;
+    }
+
+    sort(phones, phones + size, [](Phone* a, Phone* b) { return a->getPrice() < b->getPrice(); });
+    double totalCost = 0;
+    for (int i = 0; i < size; i++) {
+        output << *phones[i];
+        totalCost += phones[i]->getPrice();
+    }
+    output << "Total cost: " << totalCost << endl;
+
+    output << endl << "Phones with answering machine:\n";
+    for (int i = 0; i < size; i++) {
+        RadioPhone* rp = dynamic_cast<RadioPhone*>(phones[i]);
+        if (rp && rp->hasAnsweringMachine()) {
+            output << *phones[i];
+        }
+    }
+
+    if (phones) {
+        for (int i = 0; i < size; i++) {
+            delete phones[i];
+        }
+        delete[] phones;
+    }
+    return 0;
+}
