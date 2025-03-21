156f2c73ed5aaaafffa3cef9153a08e2d4afd166
Arsenii Litvinchuk <alitvinchuk346@gmail.com>
2025-03-21 10:55:37 +0200

    lab02 push

---
+++ lab02/LitvinchukA/Phone/Phone.cpp
@@ -0,0 +1,22 @@
+#include "Phone.h"
+
+Phone::Phone(const string& name, const string& brand, double price)
+    : name(name), brand(brand), price(price) {
+}
+
+string Phone::getName() const {
+    return name;
+}
+
+string Phone::getBrand() const {
+    return brand;
+}
+
+double Phone::getPrice() const {
+    return price;
+}
+
+ostream& operator<<(ostream& os, const Phone& phone) {
+    os << phone.name << " " << phone.brand << " " << phone.price;
+    return os;
+}
