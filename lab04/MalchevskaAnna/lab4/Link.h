aae9ddb4935c29134d8c462d75c897c444394e6d
Anna4511 <malchevskanna@gmail.com>
2025-03-21 09:47:25 +0200

    added hw04

---
+++ lab04/MalchevskaAnna/lab4/Link.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "Student.h"
+using namespace std;
+
+struct Link {
+    Link* next;
+    virtual ~Link() {}
+protected:
+    Link() : next(nullptr) {}
+};
+struct Student_Link : Link {
+    Student data;
+    Student_Link(const Student& s);
+};
---
+++ lab04/MalchevskaAnna/lab4/Student_Manager.cpp
@@ -0,0 +1,87 @@
+﻿#include "Student_Manager.h"
+
+void Student_Manager::addStudent(const Student& s) {
+    studentsList.addStudent(s);
+    cout << "Student added successfully!" << endl;
+}
+
+void Student_Manager::removeStudent(const string& name) {
+    if (studentsList.empty()) {
+        cout << "No students in the list. Nothing to remove." << endl;
+        return;
+    }
+    if (studentsList.removeStudentByName(name)) {
+        cout << "Student " << name << " removed successfully." << endl;
+    }
+    else {
+        cout << "Student " << name << " not found." << endl;
+    }
+}
+
+void Student_Manager::findStudentByName(const string& name) const {
+    if (studentsList.empty()) {
+        cout << "No students in the list." << endl;
+        return;
+    }
+    Student_Link* studentLink = studentsList.findStudentByName(name);
+    if (studentLink) {
+        cout << "Student found: ";
+        studentLink->data.print();
+    }
+    else {
+        cout << "Student " << name << " not found." << endl;
+    }
+}
+
+void Student_Manager::findStudentByGroup(const string& group) const {
+    if (studentsList.empty()) {
+        cout << "No students in the list." << endl;
+        return;
+    }
+    Student_Link* studentLink = studentsList.findStudentByGroup(group);
+    if (studentLink) {
+        cout << "Student found: ";
+        studentLink->data.print();
+    }
+    else {
+        cout << "No students found in group " << group << "." << endl;
+    }
+}
+
+void Student_Manager::findStudentByAddress(const string& address) const {
+    if (studentsList.empty()) {
+        cout << "No students in the list." << endl;
+        return;
+    }
+    Student_Link* studentLink = studentsList.findStudentByAddress(address);
+    if (studentLink) {
+        cout << "Student found: ";
+        studentLink->data.print();
+    }
+    else {
+        cout << "No students found with address " << address << "." << endl;
+    }
+}
+
+void Student_Manager::updateStudent(const string& name, const Student& newData) {
+    if (studentsList.empty()) {
+        cout << "No students in the list. Nothing to update." << endl;
+        return;
+    }
+    Student_Link* studentLink = studentsList.findStudentByName(name);
+    if (studentLink) {
+        studentLink->data = newData;
+        cout << "Student " << name << " updated successfully." << endl;
+    }
+    else {
+        cout << "Student " << name << " not found." << endl;
+    }
+}
+
+void Student_Manager::printAllStudents() const {
+    if (studentsList.empty()) {
+        cout << "No students in the list." << endl;
+        return;
+    }
+    studentsList.printAll();
+}
