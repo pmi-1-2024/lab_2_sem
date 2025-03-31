52d5a8e398904885a0ec81e7b83d5c534b1c2562
q-u-a-n-t-i-k <bagenmar@gmail.com>
2025-03-31 20:35:14 +0300

    lab4

--- lab04/Bahen/main.cpp
+++ lab04/Bahen/main.cpp
@@ -1,74 +1,17 @@
-
-#include <iostream>
+#include "s_manager.h"
 using namespace std;
 
-class Student {
-private:
-    string name;
-    int age;
-    string group;
-public:
-    Student(){
-    	name = "";
-    	age = -1;
-    	group = 1;
-    }
-};
-
-class Link{
-public:
-	Link * next;
-	Link * priv;
-public:
-	Link(){next = nullptr;priv = nullptr;}
-	void set_next(Link * l){
-		next = l;
-	}
-	void set_priv(Link * l){
-		priv = l;
-	}
-	virtual ~ Link();
-};
-
-class SLink: public Link{
-protected:
-	Student val;
-public:
-	SLink():Link(){};
-	SLink(Student s):Link(){val = s;};
-	~ SLink();
-};
-
-class List{
-protected:
-	Link * head;
-public:
-	List(){head = nullptr;}
-	bool isEmpty(){return head == nullptr;}
-	virtual Link * top(){return head;}
-	void push(Link * new_e){
-			new_e->priv = head;
-			head -> next = new_e;
-			head = new_e;
-	}
-	void pop(){
-		if(head != nullptr){
-			Link * buff = head;
-			head = buff->priv;
-			delete buff;
-		}
-	}
-	virtual ~ List();
-};
-
-class StudentList: public List{
-public:
-	StudentList():List(){};
-	void Add(const Student& s ){List::push(new SLink(s));}
-	~StudentList(){};
-};
-
-
 int main() {
+	srand(time(0));
+	StudentManager m;
+	int n;
+	cout << "Number: ";
+	cin >> n;
+	for(int i =0; i <n; i++){
+		Student s;
+		cin >> s;
+		m.add(s);
+	}
 
+	m.printAll();
 }
