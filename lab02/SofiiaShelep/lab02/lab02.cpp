986b60327e53408a10d6a265bb6d6cbb026bc34b
SofiiaShelep <sshelep5@gmail.com>
2025-04-01 16:11:37 +0300

    correction

--- lab02/SofiiaShelep/lab02/lab02.cpp
+++ lab02/SofiiaShelep/lab02/lab02.cpp
@@ -6,91 +6,71 @@
 
 using namespace std;
 
-int main()
-{
-	ifstream file1("file1.txt");
-	ifstream file2("file2.txt");
+void readPhones(ifstream& file, Phone** phones, int& index, int count) {
+    for (int i = 0; i < count; i++, index++) {
+        string type;
+        file >> type;
+        if (type == "Mobile") {
+            phones[index] = new MobilePhone();
+        }
+        else if (type == "Radio") {
+            phones[index] = new RadioPhone();
+        }
+        file >> *phones[index];
+    }
+}
 
-	int count1 = 0;
-	int count2 = 0;
+int main() {
+    ifstream file1("file1.txt");
+    ifstream file2("file2.txt");
 
-	file1 >> count1;
-	file2 >> count2;
+    int count1 = 0, count2 = 0;
+    file1 >> count1;
+    file2 >> count2;
 
-	Phone** phones = new Phone * [count1 + count2];
+    Phone** phones = new Phone * [count1 + count2];
+    int index = 0;
 
-	for (int i = 0; i < count1; i++)
-	{
-		string type;
-		file1 >> type;
-		if (type == "Mobile")
-		{
-			phones[i] = new MobilePhone();
-			file1 >> *phones[i];
-		}
-		else if (type == "Radio")
-		{
-			phones[i] = new RadioPhone();
-			file1 >> *phones[i];
-		}
-	}
+    readPhones(file1, phones, index, count1);
+    readPhones(file2, phones, index, count2);
 
+    for (int i = 0; i < count1 + count2 - 1; i++) {
+        for (int j = 0; j < count1 + count2 - i - 1; j++) {
+            if (*phones[j] > *phones[j + 1]) {
+                Phone* temp = phones[j];
+                phones[j] = phones[j + 1];
+                phones[j + 1] = temp;
+            }
+        }
+    }
 
-	for (int i = count1; i < count1 + count2; i++)
-	{
-		string type;
-		file2 >> type;
-		if (type == "Mobile")
-		{
-			phones[i] = new MobilePhone();
-			file2 >> *phones[i];
-		}
-		else if (type == "Radio")
-		{
-			phones[i] = new RadioPhone();
-			file2 >> *phones[i];
-		}
-	}
+    ofstream file3("file3.txt");
+    for (int i = 0; i < count1 + count2; i++) {
+        file3 << *phones[i] << endl;
+    }
 
-	for (int i = 0; i < count1 + count2 - 1; i++) {
-		for (int j = 0; j < count1 + count2 - i - 1; j++) {
-			if (*phones[j] > *phones[j + 1]) {
-				Phone* temp = phones[j];
-				phones[j] = phones[j + 1];
-				phones[j + 1] = temp;
-			}
-		}
-	}
-	ofstream file3("file3.txt");
-	for (int i = 0; i < count1 + count2; i++)
-	{
-		file3 << *phones[i] << endl;
-	}
+    int sum = 0;
+    for (int i = 0; i < count1 + count2; i++) {
+        sum += phones[i]->getPrice();
+    }
+    file3 << sum << endl;
 
-	int sum = 0;
-	for (int i = 0; i < count1 + count2; i++)
-	{
-		sum += phones[i]->getPrice();
-	}
-	file3 << sum << endl;
+    for (int i = 0; i < count1 + count2; i++) {
+        if (auto* radioPhone = dynamic_cast<RadioPhone*>(phones[i])) {
+            if (radioPhone->getAnsweringMachine()) {
+                file3 << *phones[i] << endl;
+            }
+        }
+    }
 
-	for (int i = 0; i < count1 + count2; i++)
-	{
-		if (dynamic_cast<RadioPhone*>(phones[i]))
-		{
-			if (dynamic_cast<RadioPhone*>(phones[i])->getAnsweringMachine())
-			{
-				file3 << *phones[i] << endl;
-			}
-		}
-	}
-	file1.close();
-	file2.close();
-	file3.close();
-	for (int i = 0; i < count1 + count2; i++)
-	{
-		delete phones[i];
-	}
+    file1.close();
+    file2.close();
+    file3.close();
 
-	return 0;
+    for (int i = 0; i < count1 + count2; i++) {
+        delete phones[i];
+    }
+    delete[] phones;
+
+    return 0;
 }
