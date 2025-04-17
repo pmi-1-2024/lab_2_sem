156eea05f4a31f94e7154f72967d06c196cf88dc
Anna Malchevska <malchevskanna@gmail.com>
2025-04-17 14:36:14 +0300

    Update Student.cpp

--- lab04/MalchevskaAnna/lab4/Student.cpp
+++ lab04/MalchevskaAnna/lab4/Student.cpp
@@ -7,4 +7,18 @@ Student::Student(const string& n, int a, const string& m,
 void Student::print() const {
     cout << "Name: " << name << ", Age: " << age << ", Major: " << major
         << ", Address: " << address << ", Group: " << group << endl;
+
+    istream& operator>>(istream& is, Student& s) {
+    cout << "Enter name: ";
+    getline(is, s.name);
+    cout << "Enter age: ";
+    is >> s.age;
+    is.ignore(); 
+    cout << "Enter major: ";
+    getline(is, s.major);
+    cout << "Enter address: ";
+    getline(is, s.address);
+    cout << "Enter group: ";
+    getline(is, s.group);
+    return is;
 }
