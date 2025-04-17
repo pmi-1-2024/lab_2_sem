8ceb5769613e6ed499bf60009ddb347e46771ddc
Anna4511 <malchevskanna@gmail.com>
2025-04-17 17:08:52 +0300

    add lab06

---
+++ lab06/MalchevskaAnn/Operations.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <fstream>
+#include "SpecialTransport.h"
+
+void startMenu();
+void readFromFile(ifstream& file, Transport<string>** transports, int& count);
+void distributeLoad(double totalLoad, Transport<string>** transports, int count);
+void printDistribution(Transport<string>** transports, int count);
+void applyDiscount(Transport<string>** transports, int count, int index);
+void writeToFile(const string& filename, Transport<string>** transports, int count);
+void printAllTransports(Transport<string>** transports, int count);
+
