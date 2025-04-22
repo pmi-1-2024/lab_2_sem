b4c94c923bb51ecaa2f27091c03d8e0d8a808cd8
Kryshchyshyn Oleksii <oozoleksa@gmail.com>
2025-04-22 14:30:43 +0300

    added realization of operands

--- lab3/lab3/MobilePhone.cpp
+++ lab3/lab3/MobilePhone.cpp
@@ -7,14 +7,21 @@ MobilePhone::MobilePhone() : Phone() {
     memory = 0;
 }
 //void MobilePhone::display() const {
-//    cout << "Mobile Phone: " << name << ", " << brand << ", " << price << ", " << color << ", " << memory << "GB" << endl;
+//   cout << "Mobile Phone: " << name << ", " << brand << ", " << price << ", " << color << ", " << memory << "GB" << endl;
 //
 //
 //}
 
+ostream& operator<<(ostream& os, const MobilePhone& phone) {
+    os << "Name: " << phone.name << "\n";
+    os << "Brand: " << phone.brand << "\n";
+    os << "Price: " << phone.price << "\n";
+    os << "Color: " << phone.color << "\n";
+    os << "Memory: " << phone.memory << " GB\n";
+    return os;
+}
 
-
-//istream& operator>>(istream& is, MobilePhone& phone) {
-//    is >> phone.name >> phone.brand >> phone.price >> phone.color >> phone.memory;
-//    return is;
-//}
+istream& operator>>(istream& is, MobilePhone& phone) {
+   is >> phone.name >> phone.brand >> phone.price >> phone.color >> phone.memory;
+    return is;
+}
