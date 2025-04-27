f33538f5f0c8087d3129e311f958d68425ee9583
SofiiaShelep <sshelep5@gmail.com>
2025-04-27 18:21:12 +0300

    lab6 with exceptions

---
+++ lab06/SofiiaShelep/ConsoleApplication18/BaseTransport.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class BaseTransport
+{
+public:
+    virtual void print() = 0;
+    virtual double getPrice() = 0;
+    virtual void setDestination(const string& destination) = 0; 
+    virtual ~BaseTransport() {}
+};
