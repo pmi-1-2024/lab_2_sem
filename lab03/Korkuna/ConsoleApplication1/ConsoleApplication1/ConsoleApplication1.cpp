ce12b42e05db34f515ac103eba3bcab989c6b0dd
aakorkuna <aakorkuna@gmail.com>
2025-05-02 14:09:34 +0300

    lab 3

--- lab03/Korkuna/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ lab03/Korkuna/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,20 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <fstream>
+#include "phone.h"
+#include "m_phone.h"
+#include "r_phone.h"
+#include "c_phone.h"
+#include "functions.h"
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+	int n = 10;
+	int index = 0;
+	Phone** phones = new Phone * [n];
+
+	read("data.txt", phones, index);
+	cout << 1;
+	search(phones, index);
+
+	return 1;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
