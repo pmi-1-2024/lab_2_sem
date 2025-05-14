87720a800332761cb58deb7a155f98a463426dc9
Hrynchyshyn <hrynchyshyn555@gmail.com>
2025-05-14 21:00:50 +0300

    lab08

---
+++ Lab08/Hrynchyshyn/Material.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Material {
+private:
+    string date;
+    string name;
+    double volume;
+    double cost;
+
+public:
+    Material();
+    Material(const string&, const string&, double, double);
+
+    string getDate() const;
+    string getName() const;
+    double getVolume() const;
+    double getCost() const;
+
+    void print(ostream&) const;
+    bool operator<(const Material&) const;
+
+
+    friend istream& operator>>(istream& in, Material& m);
+    friend ostream& operator<<(ostream& out, const Material& m);
+};
