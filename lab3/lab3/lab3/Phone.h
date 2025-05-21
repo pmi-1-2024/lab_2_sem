9d01e954d59121b83aec0dbd173b4576646e21e9
Volovyk <vertabor@gmail.com>
2025-05-21 19:05:06 +0300

    pravki

---
+++ lab3/lab3/lab3/Phone.h
@@ -0,0 +1,39 @@
+#ifndef PHONE_H
+#define PHONE_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Phone {
+protected:
+    string name;
+    string brand;
+    double price;
+
+public:
+
+
+    Phone() : name(""), brand(""), price(0) {}
+    virtual ~Phone() {}
+
+    virtual void read(istream& in) = 0;
+
+
+       virtual void write(ostream& out) const = 0;
+
+       double getPrice() const { return price; }
+       string getName() const { return name; }
+
+        friend istream& operator>>(istream& in, Phone& p) {
+        p.read(in);
+        return in;
+    }
+
+        friend ostream& operator<<(ostream& out, const Phone& p) {
+        p.write(out);
+        return out;
+    }
+};
+
+#endif
