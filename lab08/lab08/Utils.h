e19ba3e81b990a41bbba3e1d1dfa9e1d14261577
Yuliia <nazarovau618@gmail.com>
2025-05-23 18:07:51 +0300

    update Utils.h

--- lab08/lab08/Utils.h
+++ lab08/lab08/Utils.h
@@ -4,10 +4,6 @@
 #include <map>
 using namespace std;
 
-using Material = deque<string>;
-using MaterialEntry = Material;
-using BrigadeMap = map<int, pair<string, deque<MaterialEntry>>>;
-
 void readData(const string& filename, BrigadeMap& brigades, map<int, double>& costs);
 void writeSortedBrigades(const BrigadeMap& brigades, const map<int, double>& costs, const string& filename);
 void writeMinCostBrigades(const map<int, double>& costs, const BrigadeMap& brigades, const string& filename);
