153c8b9e0b1e214f6bcb4ee7c34d795774b25f1c
sashabiletsky03 <48645443+sashabiletsky03@users.noreply.github.com>
2025-05-23 12:31:21 +0300

    Update lab9.cpp

--- lab 9 sasha biletskiy/lab9.cpp
+++ lab 9 sasha biletskiy/lab9.cpp
@@ -3,19 +3,9 @@
 #include <iterator>
 #include <vector>
 #include <algorithm>
-#include <functional>
 #include <map>
 
 using namespace std;
-
-
-struct IsChar {
-    char ch;
-    IsChar(char c) : ch(c) {}
-    bool operator()(const string& s) const {
-        return s == string(1, ch);
-    }
-};
 
 int main() {
     ifstream inFile("input.txt");
@@ -27,22 +17,33 @@ int main() {
     }
 
     int n;
-    inFile >> n;
-    istream_iterator<string> inIter(inFile), eos;
-    vector<string> symbols(inIter, eos);
+    if (!(inFile >> n)) {
+        cerr << "Помилка зчитування кількості символів." << endl;
+        return 1;
+    }
+
+    istream_iterator<char> inIter(inFile), eos;
+    vector<char> symbols;
+    copy_n(inIter, n, back_inserter(symbols));
+
+    if (symbols.size() < n) {
+        cerr << "Недостатньо символів у файлі." << endl;
+        return 1;
+    }
+
     vector<char> targets = { '+', '-', '*' };
     map<char, int> counts;
 
     for (char ch : targets) {
-        int count = count_if(symbols.begin(), symbols.end(), IsChar(ch));
-        counts[ch] = count;
+        counts[ch] = count_if(symbols.begin(), symbols.end(),
+                              [ch](char c) { return c == ch; });
     }
 
     outFile << "Кількість '+': " << counts['+'] << endl;
     outFile << "Кількість '-': " << counts['-'] << endl;
     outFile << "Кількість '*': " << counts['*'] << endl;
     outFile << "Загальна кількість символів '+', '-', '*': "
-        << (counts['+'] + counts['-'] + counts['*']) << endl;
+            << (counts['+'] + counts['-'] + counts['*']) << endl;
 
     return 0;
 }
