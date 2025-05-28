f177f432dfe4317b5d690eef023f99fef0f54ea1
aakorkuna <aakorkuna@gmail.com>
2025-05-28 18:08:48 +0300

    lab4

---
+++ lab04/Korkuna/s_link.h
@@ -0,0 +1,11 @@
+#include "link.h"
+#include "student.h"
+using namespace std;
+
+class SLink : public Link {
+public:
+	Student val;
+	SLink() :Link() {};
+	SLink(Link* l, Student s) :Link(l) { val = s; }
+	~SLink() {};
+};
